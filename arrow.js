53284648255c267bf68794cec4c2414e731c75bb
serdjd <sergiodobladomunoz@gmail.com>
2024-03-06 14:46:02 +0100

    BUG: no funciona nada

---
+++ arrow.js
@@ -0,0 +1,9 @@
+const divs = document.getElementsByClassName("box");
+function arrow(){
+    let element = document.querySelector();
+    element = element.id;
+    let div = document.getElementById(element);
+    let x = div.offsetLeft;
+    let arrow = document.getElementById("arrow");
+    arrow.style.left = '${x}px'
+}
