962e11ea0a78cd9e0e3e46341e8a1b61dfe25275
Serdjd <sergiodobladomunoz@gmail.com>
2024-03-07 23:15:01 +0100

    UPD: ventana emergente para el reloj

--- timer.js
+++ timer.js
@@ -1,3 +1,8 @@
 function timer(){
-    window.open("time_select.html");
-}
+    Swal.fire({
+        html: '<iframe src="time_select.html" style="width: 18em; height: 19em; border: none; margin: 0px;"></iframe>',
+        showConfirmButton: false,
+        padding: 0,
+        customClass: 'sweetalert-lg',
+      });
+  }
