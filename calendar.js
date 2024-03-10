1dc5d73d46f5b005321f65a2fec53ba19e98dc17
Serdjd <sergiodobladomunoz@gmail.com>
2024-03-10 22:53:14 +0100

    UPD: ventana con sweetAlert

--- calendar.js
+++ calendar.js
@@ -1,21 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var calendarEl = document.getElementById('calendar');
-	var iframeEl = document.getElementById('miIframe');
+  var calendarEl = document.getElementById('calendar');
+  var iframeEl = document.getElementById('miIframe');
 
-	var calendar = new FullCalendar.Calendar(calendarEl, {
-	  initialView: 'dayGridMonth',
-	  headerToolbar: {
-		center: 'addEventButton'
-	  },
-	  customButtons: {
-		addEventButton: {
-		  text: 'add event...',
-		  click: function() {
-			iframeEl.style.display = 'block'
-			
-		  }
-		}
-	  } 
-	});
-	calendar.render();
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      center: 'addEventButton'
+    },
+    customButtons: {
+      addEventButton: {
+        text: 'add event...',
+        click: function() {
+          Swal.fire({
+            html: '<iframe src="new_event.html" style="width: 25em; height: 29em; border: none; margin: 0px;"></iframe>',
+            showConfirmButton: false,
+            padding: 0,
+            customClass: 'sweetalert-lg',
+          });
+        }
+      }
+    }
+  });
+  calendar.render();
 });
+
--- h1-editable.js
+++ h1-editable.js
@@ -1,8 +1,20 @@
 function h1(){
     const h1 = document.querySelector('h1');
-
     h1.addEventListener('click', () => {
     h1.contentEditable = true;
     });
-
+    h1.addEventListener('keydown',letterCount());
 }
+function letterCount(event){
+    let words = count(h1);
+    console.log(words)
+    if(words >= 18){
+        event.preventDefault();
+    }
+}
+function count(words){
+    var word = words.textContent;
+    let letters = word.split("");
+    return letters.length;
+}
+
