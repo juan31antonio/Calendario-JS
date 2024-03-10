f9ed8f18e6f7197e67927f74e360673f2678e141
juan31antonio <juanantoniocorder32@gmail.com>
2024-03-11 00:40:02 +0100

    calendario add events

--- calendar.js
+++ calendar.js
@@ -1,26 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-  var iframeEl = document.getElementById('miIframe');
+    var calendarEl = document.getElementById('calendar');
+    var iframeEl = document.getElementById('miIframe');
 
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    headerToolbar: {
-      center: 'addEventButton'
-    },
-    customButtons: {
-      addEventButton: {
-        text: 'add event...',
-        click: function() {
-          Swal.fire({
-            html: '<iframe src="new_event.html" style="width: 25em; height: 29em; border: none; margin: 0px;"></iframe>',
-            showConfirmButton: false,
-            padding: 0,
-            customClass: 'sweetalert-lg',
-          });
-        }
-      }
-    }
-  });
-  calendar.render();
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        dateClick: function(info) {
+			const swal = Swal.fire({
+				html: '<iframe src="new_event.html" style="width: 25em; height: 29em; border: none; margin: 0px;"></iframe>',
+				showConfirmButton: false,
+				padding: 0,
+				customClass: 'sweetalert-lg',
+			}).finally(() => {
+				parseEvent(calendar)
+			  });
+		  },
+    });
+	window.addEventListener("message", (event) => {
+		if (event.data.type === "swal-confirm") {
+		  parseEvent(calendar);
+		  swal.close();
+		}
+	  });
+    calendar.render();
 });
 
+function parseEvent(calendar){
+	var parseo = JSON.parse(localStorage.getItem('evento'));
+	calendar.addEvent({
+		title: 'pwpw',
+		description: parseo.description,
+		start: '2024-03-11'
+	});
+	console.log(parseo);
+}
---
+++ eventData.js
@@ -0,0 +1,24 @@
+
+function eventData(){
+    var tittle = document.querySelector('h1').textContent;
+    var description = document.querySelector('input').value;
+    var time = document.getElementById('time').textContent;
+    const event = {
+        title: tittle,
+        description: description,
+        start: time
+      }
+      localStorage.setItem("evento",JSON.stringify(event));
+      
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const miBoton = document.getElementById("close");
+
+miBoton.addEventListener("click", () => {
+  // Función que se ejecuta al hacer clic en el botón
+  window.parent.postMessage({
+    type: "swal-confirm",
+  }, "*");
+});
+});
